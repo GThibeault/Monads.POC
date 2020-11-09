4434efaea71a57ff1422bfca5cc18f71db818083
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-09 19:56:22 -0300

    IMonad interface

---
+++ Monads.POC.Common/Monads/Interfaces/IMonad.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Common.Interfaces
+{
+    public interface IMonad<T>
+    {
+        public IMonad<TNext> Bind<TNext>(Func<T, IMonad<TNext>> bindFunc);
+    }
+}
