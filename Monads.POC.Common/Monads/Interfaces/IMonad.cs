446f061aae98a64ba36fd8f398d86e2954176f54
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-09 21:05:34 -0300

    IMonad with visitor

--- Monads.POC.Common/Monads/Interfaces/IMonad.cs
+++ Monads.POC.Common/Monads/Interfaces/IMonad.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Monads.POC.Common.Monads.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Monads.POC.Common.Interfaces
 {
-    public interface IMonad<T>
+    public interface IMonad<TValue>
     {
-        public IMonad<TNext> Bind<TNext>(Func<T, IMonad<TNext>> bindFunc);
+        public IMonad<TNext> Bind<TNext>(Func<TValue, IMonad<TNext>> bindFunc);
+
+        public TReturn Accept<TReturn>(IMonadVisitor<TValue, TReturn> visitor);
     }
 }
---
+++ Monads.POC.Common/Monads/Interfaces/IMonadVisitor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Common.Monads.Interfaces
+{
+    public interface IMonadVisitor<TValue, TReturn>
+    {
+        public TReturn VisitValue(TValue value);
+    }
+}
