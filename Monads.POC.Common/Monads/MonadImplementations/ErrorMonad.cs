d3018174bcc7931d27db02388a9e02b58b2f943e
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-09 23:21:54 -0300

    ErrorMonad

---
+++ Monads.POC.Common/Monads/MonadImplementations/ErrorMonad.cs
@@ -0,0 +1,28 @@
+﻿using Monads.POC.Common.Interfaces;
+using Monads.POC.Common.Monads.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Common.Monads.MonadImplementations
+{
+    public class ErrorMonad<TValue> : IMonad<TValue>
+    {
+        protected String ErrorMessage { get; }
+
+        public ErrorMonad(String errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+
+        public TReturn Accept<TReturn>(IMonadVisitor<TValue, TReturn> visitor)
+        {
+            return visitor.VisitError(ErrorMessage);
+        }
+
+        public IMonad<TNext> Bind<TNext>(Func<TValue, IMonad<TNext>> bindFunc)
+        {
+            return new ErrorMonad<TNext>(ErrorMessage);
+        }
+    }
+}
