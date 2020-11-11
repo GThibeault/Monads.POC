a7199de04f7480ff9a262b5c41bbb198a270efdf
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-11 01:32:30 -0300

    Added simple ILogger implementation for tests

---
+++ Monads.POC.Tests/LoggerMonadTests/TestLogger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Tests.LoggerMonadTests
+{
+    #pragma warning disable CA1062 // Validate arguments of public methods
+    public class TestLogger : ILogger
+    {
+        public ICollection<String> LoggedValues { get; }
+
+        public TestLogger()
+        {
+            LoggedValues = new List<String>();
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, String> formatter)
+        {
+            String loggedValue = formatter(state, exception);
+            LoggedValues.Add(loggedValue);
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boolean IsEnabled(LogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #pragma warning restore CA1062 // Validate arguments of public methods
+}
