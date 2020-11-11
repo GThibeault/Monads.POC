74af891140337ff60fe453491205dd6ed5e6a463
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-11 02:47:43 -0300

    Added "safe" value monad which catches exceptions during bind

---
+++ Monads.POC.Common/Monads/MonadImplementations/SafeValueMonad.cs
@@ -0,0 +1,25 @@
+﻿using Monads.POC.Common.Interfaces;
+using Monads.POC.Common.Monads.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Common.Monads.MonadImplementations
+{
+    public class SafeValueMonad<TValue> : ValueMonad<TValue>
+    {
+        public SafeValueMonad(TValue value) : base(value) {}
+
+        public override IMonad<TNext> Bind<TNext>(Func<TValue, IMonad<TNext>> bindFunc)
+        {
+            try
+            {
+                return base.Bind(bindFunc);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorMonad<TNext>(ex.Message);
+            }
+        }
+    }
+}
