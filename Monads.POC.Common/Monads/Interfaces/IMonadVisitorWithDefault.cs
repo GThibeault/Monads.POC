aee3b72099cf3445afeadbf89322a1e66e2c1861
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-11 16:18:38 -0300

    Added XML comments for the IMonadVisitor with default visit.

--- Monads.POC.Common/Monads/Interfaces/IMonadVisitorWithDefault.cs
+++ Monads.POC.Common/Monads/Interfaces/IMonadVisitorWithDefault.cs
@@ -4,11 +4,20 @@ using System.Text;
 
 namespace Monads.POC.Common.Monads.Interfaces
 {
+    /// <summary>
+    /// Visitor pattern for monads, with a generic visit method.
+    /// Each particular visit method provides a default implementation calling the generic visit.
+    /// </summary>
+    /// <typeparam name="TValue">Value of the visited monad.</typeparam>
+    /// <typeparam name="TReturn">The return type of all visits.</typeparam>
     public interface IMonadVisitorWithDefault<TValue, TReturn> : IMonadVisitor<TValue, TReturn>
     {
         TReturn IMonadVisitor<TValue, TReturn>.VisitValue(TValue value) => VisitDefault();
         TReturn IMonadVisitor<TValue, TReturn>.VisitError(String errorMessage) => VisitDefault();
 
+        /// <summary>
+        /// Default visit called when a specific visit type is not implemented.
+        /// </summary>
         public TReturn VisitDefault();
     }
 }
