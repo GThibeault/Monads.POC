c2be429c6cab4b05f4a7a0be37ae65f1093ab844
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-10 23:07:43 -0300

    Process Monad tests

---
+++ Monads.POC.Tests/ProcessMonadTests/AssertProcessVisitor.cs
@@ -0,0 +1,30 @@
+﻿using Monads.POC.Common.Monads.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Tests.ProcessMonadTests
+{
+    public class AssertProcessVisitor<TValue> : IMonadVisitor<TValue, Boolean>
+    {
+        public ExpectedMonad ExpectedMonadType { get; set; }
+        public Object ExpectedValue { get; set; }
+
+        public Boolean VisitValue(TValue value)
+        {
+            Assert.AreEqual(ExpectedMonad.Value, ExpectedMonadType);
+            Assert.AreEqual(ExpectedValue, value);
+
+            return true;
+        }
+
+        public Boolean VisitError(String errorMessage)
+        {
+            Assert.AreEqual(ExpectedMonad.Error, ExpectedMonadType);
+            Assert.AreEqual(ExpectedValue, errorMessage);
+
+            return true;
+        }
+    }
+}
---
+++ Monads.POC.Tests/ProcessMonadTests/ExpectedMonad.cs
@@ -0,0 +1,15 @@
+﻿using Monads.POC.Common.Monads.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Tests.ProcessMonadTests
+{
+    public enum ExpectedMonad
+    {
+        Invalid,
+        Value,
+        Error
+    }
+}
