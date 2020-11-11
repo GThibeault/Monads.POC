470e1992d55b3d5d8fc7891f4d48ced25cc097b1
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-11 15:37:08 -0300

    Added XML comments for the ErrorMonad

--- Monads.POC.Common/Monads/MonadImplementations/ErrorMonad.cs
+++ Monads.POC.Common/Monads/MonadImplementations/ErrorMonad.cs
@@ -6,6 +6,13 @@ using System.Text;
 
 namespace Monads.POC.Common.Monads.MonadImplementations
 {
+    /// <summary>
+    /// A monad representing an error in execution.
+    /// Bind operations propagate the error without calling the input functions.
+    /// </summary>
+    /// <typeparam name="TValue">The undeyling type of the monad. 
+    /// Keep in mind no value is actually stored by the error monad, though.
+    /// </typeparam>
     public class ErrorMonad<TValue> : IMonad<TValue>
     {
         protected String ErrorMessage { get; }
@@ -15,12 +22,23 @@ namespace Monads.POC.Common.Monads.MonadImplementations
             ErrorMessage = errorMessage;
         }
 
-        public TReturn Accept<TReturn>(IMonadVisitor<TValue, TReturn> visitor)
+        /// <summary>
+        /// </summary>
+        /// <typeparam name="TReturn"></typeparam>
+        /// <param name="visitor"></param>
+        /// <returns></returns>
+        public virtual TReturn Accept<TReturn>(IMonadVisitor<TValue, TReturn> visitor)
         {
             return visitor.VisitError(ErrorMessage);
         }
 
-        public IMonad<TNext> Bind<TNext>(Func<TValue, IMonad<TNext>> bindFunc)
+        /// <summary>
+        /// Propagates the error without calling the input function.
+        /// </summary>
+        /// <typeparam name="TNext">Type of the following monad.</typeparam>
+        /// <param name="bindFunc">The bound function, though it won't actually be called.</param>
+        /// <returns>An error monad of the corresponding type with the same error message.</returns>
+        public virtual IMonad<TNext> Bind<TNext>(Func<TValue, IMonad<TNext>> bindFunc)
         {
             return new ErrorMonad<TNext>(ErrorMessage);
         }
