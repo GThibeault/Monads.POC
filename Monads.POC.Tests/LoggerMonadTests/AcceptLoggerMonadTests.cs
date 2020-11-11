542fe8d74c4cda95d1005f18bb9468a005e8f393
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-11 02:13:20 -0300

    Tests for the accept operation of the logger monad and a mocked visitor

---
+++ Monads.POC.Tests/LoggerMonadTests/AcceptLoggerMonadTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using Monads.POC.Common.Monads.MonadImplementations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monads.POC.Tests.LoggerMonadTests
+{
+    public class AcceptLoggerMonadTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AcceptWithValueMonadIsProperlyLogged()
+        {
+            var logger = new TestLogger();
+
+            _ = new LoggerMonad<Int32>(new ValueMonad<Int32>(0), logger)
+                .Bind(val => new ValueMonad<Int32>(2020))
+                .Accept(new NullVisitor<Int32>());
+
+            Assert.IsNotNull(logger.LoggedValues);
+            Assert.IsTrue(logger.LoggedValues[3].Contains("accept", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(logger.LoggedValues[3].Contains("2020", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(logger.LoggedValues[3].Contains("value", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        [Test]
+        public void ConstructorWithErrorMonadIsProperlyLogged()
+        {
+            var logger = new TestLogger();
+
+            _ = new LoggerMonad<Int32>(new ValueMonad<Int32>(2020), logger)
+                .Bind(val => new ErrorMonad<Int32>($"#{val}"))
+                .Accept(new NullVisitor<Int32>());
+
+            Assert.IsNotNull(logger.LoggedValues);
+            Assert.IsTrue(logger.LoggedValues[3].Contains("accept", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(logger.LoggedValues[3].Contains("#2020", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(logger.LoggedValues[3].Contains("error", StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
---
+++ Monads.POC.Tests/LoggerMonadTests/NullVisitor.cs
@@ -0,0 +1,13 @@
+﻿using Monads.POC.Common.Monads.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Tests.LoggerMonadTests
+{
+    public class NullVisitor<TValue> : IMonadVisitor<TValue, Boolean>
+    {
+        public Boolean VisitError(String errorMessage) => true;
+        public Boolean VisitValue(TValue value) => true;
+    }
+}
