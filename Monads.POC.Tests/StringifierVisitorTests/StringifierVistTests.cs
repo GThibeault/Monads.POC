6438b36e4d0471de358b7ea92635966afa41039d
Gabriel Thibeault <gthibeault@binit.com.ar>
2020-11-11 00:09:21 -0300

    Added stringifier visitor tests

---
+++ Monads.POC.Tests/StringifierVisitorTests/StringifierVistTests.cs
@@ -0,0 +1,30 @@
+﻿using Monads.POC.Common.Monads.MonadImplementations;
+using Monads.POC.Common.Monads.VisitorImplementations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.POC.Tests.StringifierVisitorTests
+{
+    public class StringifierVisitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void StringifiesValueMonadCorrectly()
+        {
+            var monad = new ValueMonad<Int32>(2020);
+            var visitor = new StringifierVisitor<Int32>();
+
+            var stringifyRes = monad.Accept(visitor);
+
+            Assert.IsNotNull(stringifyRes);
+            Assert.IsTrue(stringifyRes.Contains("2020", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(stringifyRes.Contains("value", StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
